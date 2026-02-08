e4dde775729c44c3125c9e710c110d896a7fd8d6
Anshika Sengar <sengaranshika94@gmail.com>
2026-02-08 13:07:19 +0530

    funmsg

---
+++ photo/script.js
@@ -0,0 +1,8 @@
+let msg=document.querySelector("h2 span")
+let img=document.querySelector("img")
+img.addEventListener("mouseenter",function(){
+    msg.textContent="doooorr ho jaa meri shizuka se 🤬🤬🤬"
+})
+img.addEventListener("mouseleave",function(){
+    msg.textContent="Good, ab doorrr hi rhiyo 😤😤😤"
+})
